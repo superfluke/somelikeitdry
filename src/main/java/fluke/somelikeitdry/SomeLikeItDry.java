8867925f9aedc599687fc37ef6557158b8fe4bd2
Unknown <anon@mail.com>
2019-02-04 20:43:15 -0500

    Fix fighting bop for genlayers

--- src/main/java/fluke/somelikeitdry/SomeLikeItDry.java
+++ src/main/java/fluke/somelikeitdry/SomeLikeItDry.java
@@ -45,7 +45,7 @@ public class SomeLikeItDry
 
 	public static final String MODID = "somelikeitdry";
 	public static final String NAME = "Some Like It Dry";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 
 
 	@Instance(MODID)
@@ -92,10 +92,14 @@ public class SomeLikeItDry
 	@SubscribeEvent 
 	public void onInitGenLayers(WorldTypeEvent.InitBiomeGens event)
 	{	
+		if (Loader.isModLoaded("biomesoplenty")) //not gonna work
+		{
+			return;
+		}
 		if (Loader.isModLoaded("thebetweenlands"))
 		{
 			int biomeSample = event.getOriginalBiomeGens()[1].getInts(0, 0, 1, 1)[0];
-			if(BetweenlandsCompat.isBetweenBiome(biomeSample))
+			if(BetweenlandsCompat.isBetweenBiome(biomeSample)) //don't run inside between lands dim
 				return;
 		}
 		String genOptions = "{\"biomeSize\":" + Configs.general.biomeSize + ",\"riverSize\":" + Configs.general.riverSize + "}";
